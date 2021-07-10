5adf82aa404383a40ba05e1be027311b5908fc98
Tyler Tucker <tylertucker55@gmail.com>
2021-07-09 20:35:43 -0600

    added formulas in.

--- Randomizer/app/src/main/java/com/example/randomizer/CoinFlip.java
+++ Randomizer/app/src/main/java/com/example/randomizer/CoinFlip.java
@@ -3,11 +3,29 @@ package com.example.randomizer;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class CoinFlip extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coin_flip);
+    }
+
+    class getFlip {
+        Random random = new Random();
+
+        int coinFlip = random.nextInt(2);
+
+        String flipResult;
+
+        class convertFlip {
+            if (coinFlip == 0) {
+                flipResult = "Heads";
+            } else {
+                flipResult = "Tails";
+            }
+        }
     }
 }
--- Randomizer/app/src/main/java/com/example/randomizer/DiceRoll.java
+++ Randomizer/app/src/main/java/com/example/randomizer/DiceRoll.java
@@ -3,11 +3,19 @@ package com.example.randomizer;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class DiceRoll extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dice_roll);
+    }
+
+    class rollDice {
+        Random random = new Random();
+
+        int randomRoll = random.nextInt(6);
     }
 }
